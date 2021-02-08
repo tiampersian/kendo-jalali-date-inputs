067b109029687db94cf49e4ebc78478e1be55882
tiampersian <tiampersian@gmail.com>
2021-02-08 23:23:42 +0330

    add update-version script

---
+++ update-version.js
@@ -0,0 +1,24 @@
+var myArgs = process.argv.slice(2);
+const version = myArgs[0];
+console.log("version:", version)
+const fs = require('fs');
+const saveFile = fs.writeFileSync;
+
+
+const libraryFolder = './projects/progress/';
+
+fs.readdir(libraryFolder, (err, files) => {
+  files.forEach(folder => {
+    setLibraryVersion(folder);
+  });
+});
+
+function setLibraryVersion(lib) {
+  const pkgJsonPath = libraryFolder + lib + "/package.json";
+  if (!fs.existsSync(pkgJsonPath)) {
+    return;
+  }
+  const json = require(pkgJsonPath);
+  json['version'] = version;
+  saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
+}
