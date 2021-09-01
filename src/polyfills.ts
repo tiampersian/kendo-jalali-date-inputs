95d571c1912199efcbb30c826063da9f2f0b52d6
tiampersian <tiampersian@gmail.com>
2021-09-01 12:50:22 +0430

    fix rtl style bug

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,7 +18,9 @@
  * BROWSER POLYFILLS
  */
 
-/** IE11 requires the following for NgClass support on SVG elements */
+/**
+ * IE11 requires the following for NgClass support on SVG elements
+ */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /**
@@ -55,10 +57,15 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+ import 'zone.js';  // Included with Angular CLI.
 
-/***************************************************************************************************
- * APPLICATION IMPORTS
+
+ /***************************************************************************************************
+  * APPLICATION IMPORTS
+  */
+/******************************************************************
+ * Load `$localize` - used if i18n tags appear in Angular templates.
  */
-
 import '@angular/localize/init';
+
+
